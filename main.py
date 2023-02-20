8a195f796a660f4bb1bac3eac6dd263ad4f2bbed
YuliaCheprasova <cheprasovaja@yandex.ru>
2023-02-20 21:11:10 +0700

    second iteration

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 def wish(self):
-    pass
+    print('Я хочу отправить (напишите кого или что) ')
+    who = input()
+    return who
 
 
 def place(self, who):
-    pass
+    print('Хочу отправить ', who, ' в (напишите любую точку мира) ')
+    where = input()
+    return where
 
 
 def time(self, who, where):
-    pass
+    print('Хочу отправить ', who, ' в ', where, ' в (напишите в какой год хотите его отправить) ')
+    when = input()
+    return when
 
 
 def request(self, who, where, when):
+    print('Отправляю ', who, ' в ', where, ' в ', when, ' год')
+
+
+def result(self):
     pass
